d7a6ef8db380f9d80bac6719404e5b2829599f18
Geraldy Leondas <geraldy.leondas@gmail.com>
2020-04-09 20:26:29 +0200

    menu icon

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './navbar.styles.css';
+import logo from '../../images/kof-xiv.png';
 
 const Navbar = () => {
   return (
@@ -10,18 +11,20 @@ const Navbar = () => {
         <span></span>
         <span></span>
         <ul id="menu">
-          <a href="/">
-            <li>Home</li>
-          </a>
-          <a href="/">
-            <li>About</li>
-          </a>
-          <a href="/">
-            <li>Info</li>
-          </a>
-          <a href="/">
-            <li>Contact</li>
-          </a>
+          <li>
+            <div className="logo">
+              <img src={logo} alt="" />
+            </div>
+          </li>
+          <li>
+            <i class="fas fa-angle-right"></i> Video
+          </li>
+          <li>
+            <i class="fas fa-angle-right"></i> KOF's Art
+          </li>
+          <li>
+            <i class="fas fa-angle-right"></i> About
+          </li>
         </ul>
       </div>
     </div>
