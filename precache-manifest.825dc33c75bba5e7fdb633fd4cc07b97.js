8660283bfdcaf170b3c1385b6835d58d408dfba4
Geraldy Leondas <geraldy.leondas@gmail.com>
2020-04-20 19:17:57 +0200

    Updates

---
+++ precache-manifest.825dc33c75bba5e7fdb633fd4cc07b97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397028354ae32a1dee3e3291607987e2",
+    "url": "/kof/index.html"
+  },
+  {
+    "revision": "6509c88cb3800e30784e",
+    "url": "/kof/static/css/main.d212cbcb.chunk.css"
+  },
+  {
+    "revision": "d4370082b4d66928e6b6",
+    "url": "/kof/static/js/2.7381cfcc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kof/static/js/2.7381cfcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6509c88cb3800e30784e",
+    "url": "/kof/static/js/main.cf82088e.chunk.js"
+  },
+  {
+    "revision": "d3a4b8931bbbe8aa1d9f",
+    "url": "/kof/static/js/runtime-main.fccb4864.js"
+  },
+  {
+    "revision": "e639e0fdb2fcbdf6e734230cb59accbd",
+    "url": "/kof/static/media/kof-xiv.e639e0fd.png"
+  }
+]);
